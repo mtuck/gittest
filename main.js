ad3b1175f4e7962b2b98f2277ffe864910832274
Martin Tuck <Mtuck@una.edu>
2017-01-18 10:50:38 -0600

    Actually added the files

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ $( document).ready(function(){
     
     
 });
+
+
+$("#panzoom").panzoom({
+    $zoomIn: $(".zoom-in"),
+    $zoomOut: $(".zoom-out"),
+    $zoomRange: $(".zoom-range"),
+    $reset: $(".reset")
+});
+
+
+$(document).ready(function () {
+    $('#myModal').on('show.bs.modal', function (e) {
+        var image = $(e.relatedTarget).attr('src');
+        $(".modal-link").attr("src", image);
+    });
+});
